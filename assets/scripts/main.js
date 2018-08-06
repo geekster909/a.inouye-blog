ce2ac57fe91a421fd2ae931f6778270630c44fb6
justin <jbond@schieferchopshop.com>
2018-08-06 09:58:08 -0700

    changed indentations

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,63 +1,62 @@
 jQuery( document ).ready(function($) {
     console.log( "ready!" );
 
-	function scrollToAnchor(aid){
-	    var aTag = $("a[name='"+ aid +"']");
-	    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+    function scrollToAnchor(aid){
+        var aTag = $("a[name='"+ aid +"']");
+        $('html,body').animate({scrollTop: aTag.offset().top},'slow');
 
-	}
-	$('.hp__hero--arrow').click(function(event) {
-		scrollToAnchor('section2');
-	});
+    }
+    $('.hp__hero--arrow').click(function(event) {
+        scrollToAnchor('section2');
+    });
 
-	$('.hp__hero--slider').slick({
-		slide: 'div',
-		dots: false,
-		infinite: true,
-		autoplay: true,
-		arrows: false,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		speed: 500,
-		fade: true,
-		adaptiveHeight: true,
-		cssEase: 'linear',
-		autoplaySpeed: 5000,
-		draffable: false
-	});
+    $('.hp__hero--slider').slick({
+        slide: 'div',
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        autoplaySpeed: 5000,
+        draggable: false
+    });
 
-	function moveToSelected(element) {
-		if (element == "next") {
-			var selected = $(".selected").next();
-		} else if (element == "prev") {
-			var selected = $(".selected").prev();
-		} else {
-			var selected = element;
-		}
+    function moveToSelected(element) {
+        if (element == "next") {
+            var selected = $(".selected").next();
+        } else if (element == "prev") {
+            var selected = $(".selected").prev();
+        } else {
+            var selected = element;
+        }
 
-		var next = $(selected).next();
-		var prev = $(selected).prev();
-		var prevSecond = $(prev).prev();
-		var nextSecond = $(next).next();
+        var next = $(selected).next();
+        var prev = $(selected).prev();
+        var prevSecond = $(prev).prev();
+        var nextSecond = $(next).next();
 
-		$(selected).removeClass().addClass("selected");
+        $(selected).removeClass().addClass("selected");
 
-		$(prev).removeClass().addClass("prev");
-		$(next).removeClass().addClass("next");
+        $(prev).removeClass().addClass("prev");
+        $(next).removeClass().addClass("next");
 
-		$(nextSecond).removeClass().addClass("nextRightSecond");
-		$(prevSecond).removeClass().addClass("prevLeftSecond");
+        $(nextSecond).removeClass().addClass("nextRightSecond");
+        $(prevSecond).removeClass().addClass("prevLeftSecond");
 
-		$(nextSecond).nextAll().removeClass().addClass('hideRight');
-		$(prevSecond).prevAll().removeClass().addClass('hideLeft');
+        $(nextSecond).nextAll().removeClass().addClass('hideRight');
+        $(prevSecond).prevAll().removeClass().addClass('hideLeft');
 
-	}
+    }
 
-	$('#carousel div').click(function(e) {
-		if (!$(this).hasClass('selected')) {
-			e.preventDefault();
-			moveToSelected($(this));
-		}
-	});
+    $('#carousel div').click(function(e) {
+        if (!$(this).hasClass('selected')) {
+            e.preventDefault();
+            moveToSelected($(this));
+        }
+    });
 
 });
